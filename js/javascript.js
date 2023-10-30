367b0e501fb04f1e521e62f7fb74d0c03d88d97c
unknown <uosantos@outlook.com.br>
2023-10-30 00:17:16 -0300

    melhorias css

---
+++ js/javascript.js
@@ -0,0 +1,18 @@
+function toastAlert(icon, title) {
+  const Toast = Swal.mixin({
+    toast: true,
+    position: "center",
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener("mouseenter", Swal.stopTimer);
+      toast.addEventListener("mouseleave", Swal.resumeTimer);
+    },
+  });
+
+  Toast.fire({
+    icon: icon,
+    title: title,
+  });
+}
